d46d1cd843253d99faf0b21738eb636bdbdbad0a
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2022-02-26 16:26:41 +0800

    Update main
    main.py now only runs the flask server without doing any of the parsing

--- main.py
+++ main.py
@@ -1,75 +1,26 @@
 import flask
-from requests import get
+import cases
+import smm
+import vaccination
 from flask import render_template
-
-
-def gov():
-    '''
-    Gets live covid updates from www.moh.gov.sg, note, these are new cases
-
-    :return: tuple(date, artCount, pcrCount, totalCount, "https://www.gov.sg/features/covid-19")
-    '''
-
-    def fetch_result():
-        moh_url = "https://www.gov.sg/features/covid-19"
-        response = get(moh_url)
-
-        return response.text
-
-    html = fetch_result()
-
-    caseStart = html.find('<span style="font-family: Arial; font-size: 24px; color: #000000;">')
-    caseEnd = html[caseStart + 1:].find('<span style="font-family: Arial; font-size: 24px; color: #000000;">')
-
-    caseSummary = html[caseStart: caseStart + caseEnd]
-
-    dateStart = caseSummary.find('as of')
-    dateEnd = caseSummary.find('</span></strong></span></strong><strong style="font-size: 13px;">')
-
-    date = caseSummary[dateStart + 6: dateEnd]
-
-    pcrStart = caseSummary.find('<span style="font-family: Arial; font-size: 24px; color: #008080;"><strong>')
-    pcrEnd = caseSummary[pcrStart:].find('</strong></span></td>')
-    pcrCount = caseSummary[pcrStart + 75: pcrEnd + pcrStart]
-
-    if ',' in pcrCount:
-        pcrCount = int(pcrCount.replace(',', ''))
-
-
-    artStart = caseSummary[pcrEnd:].find('<span style="font-size: 24px; color: #008080;"><strong>')
-    artEnd = caseSummary[artStart:].find('</strong></span></td>')
-    artCount = caseSummary[artStart + 135: artStart + artEnd]
-    
-    if ',' in artCount:
-        artCount = int(artCount.replace(',', ''))
-
-
-    totalCount = artCount + pcrCount
-
-    return (date, artCount, pcrCount, totalCount, "https://www.gov.sg/features/covid-19")
-
-
-def vaccReg():
-    '''
-    Vaccine registration
-    '''
-    url = 'https://appointment.vaccine.gov.sg/'
-
-
 
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
-    newCases = gov()
-    return render_template('.html', newCases = newCases)
+    newCases = cases.gov()
+    updates = smm.govupdate()
+    workplace = smm.workplace()
+    vacSignup = vaccination.vaccineRegistration()
+
+    return render_template('home.html', newCases = newCases, updates = updates, workplace = workplace, vacSignup = vacSignup)
 
 
 
-@app.route('/about')
-def about():
-    pass
+
+if __name__ == '__main__':
+    app.run()
 
 
 
